4dc96f12bda3365da6c358889d6b6cd6998ff313
PauloPortofolio <pvba02@gmail.com>
2021-07-22 08:43:34 -0600

    1) Initial commit

---
+++ src/test/java/com/tdd/parallel/AppDriverTests.java
@@ -0,0 +1,13 @@
+package com.tdd.parallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDriverTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
